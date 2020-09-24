1abfc20fad3cefc298077fbb747975c5fa0e64b0
Ibtid <ibtid011@example.com>
2020-09-24 08:51:09 +0600

    style

--- src/Carousel/BuildCarousel.js
+++ src/Carousel/BuildCarousel.js
@@ -4,16 +4,18 @@ import Card from "./Card/Card";
 const BuildCarousel = () => {
   const breakPoints = [
     { width: 1, itemsToShow: 1 },
-    { width: 500, itemsToShow: 2 },
-    { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 },
+    { width: 500, itemsToShow: 1 },
+    { width: 768, itemsToShow: 1 },
+    { width: 1200, itemsToShow: 1 },
   ];
+
   return (
     <div>
       <Carousel
         breakPoints={breakPoints}
         enableAutoPlay={true}
         autoPlaySpeed={4000}
+        showArrows={false}
       >
         <Card number="1" />
         <Card number="2" />
