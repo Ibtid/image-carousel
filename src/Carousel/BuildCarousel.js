87d7c29130033614ab2df812e4222caf313a99a6
Ibtid <ibtid011@example.com>
2020-09-24 08:09:53 +0600

    add breakPoints

--- src/Carousel/BuildCarousel.js
+++ src/Carousel/BuildCarousel.js
@@ -2,9 +2,15 @@ import React from "react";
 import Carousel from "react-elastic-carousel";
 import Card from "./Card/Card";
 const BuildCarousel = () => {
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 500, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
   return (
     <div>
-      <Carousel>
+      <Carousel breakPoints={breakPoints}>
         <Card number="1" />
         <Card number="2" />
         <Card number="3" />
